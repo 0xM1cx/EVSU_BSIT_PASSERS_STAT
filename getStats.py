f1a085eab49c6c14f64e175850436da407c5dafb
Shawn Sudaria <shawnlovecode14@gmail.com>
2022-06-24 10:51:33 +0800

    update

---
+++ getStats.py
@@ -0,0 +1,8 @@
+with open("NumberOfBSITPassers.txt", "r") as f:
+    NumberOfPassers = []
+    lines = f.readlines()
+    for i in lines:
+        if i == " ":
+            print("BlankLine")
+        else:
+            print(i)
