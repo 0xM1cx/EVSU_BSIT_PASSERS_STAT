4a99af3d48c3300a35dff436834970a73e6802b8
shawn michael sudaria <shawnlovecode14@gmail.com>
2023-01-18 02:23:41 +0800

    update

--- getStatistics.py
+++ getStatistics.py
@@ -1,5 +1,10 @@
-import matplotlib
+import matplotlib.pyplot as akonplotter
 import json
+
+# ===== TODO =====
+# + Compare the results of this program to the pdf file
+# check if the number of passers are the same
+# + Change the font size of the x axis labels to fit
 
 # Get the json data from the DATA.json file
 def getJsonData():
@@ -10,11 +15,76 @@ def getJsonData():
 
 # Get the number of students per key in the json_data dict
 def getStats(data):
-     
+    courses_dict = {
+        'BEED': 0, 
+        'BS Ind. Tech. GAP': 0,  
+        'BS Ind. Tech. E': 0, 
+        'BSF': 0,  
+        'BSMATH': 0, 
+        'BTVTED HVART': 0, 
+        'BSOA': 0, 
+        'BTVED AFA': 0,  
+        'BSIT': 0,  
+        'BSE': 0,  
+        'BS Ind. Tech. ELX': 0,  
+        'BAEL': 0,  
+        'BSID': 0,  
+        'BSCE': 0,  
+        'BSED SCI': 0, 
+        'BS Ind. Tech. CA': 0,  
+        'BPED': 0,  
+        'BSHM': 0, 
+        'BENS': 0,  
+        'BSIENG': 0,  
+        'BSND': 0,  
+        'BTVTED FSM': 0,  
+        'BSFi': 0, 
+        'BSARCH': 0,  
+        'BSMT AT': 0, 
+        'BSA': 0, 
+        'BSED MATH': 0, 
+        'BSEE': 0, 
+        'BSM': 0,  
+        'BSECON': 0, 
+        'BSME': 0, 
+        'BSCHE': 0, 
+        'BSECE': 0,  
+        'BTVTED GFD': 0, 
+        'BSAGRI': 0, 
+        'BSCHEM': 0, 
+        'BCAEd': 0, 
+        'BSSTAT': 0, 
+        'BTLED HE': 0, 
+        'BSE ENGLISH': 0, 
+        'BTLED IA': 0, 
+        'BSGE': 0, 
+        'BSMT MS': 0, 
+        'BS Ind. Tech. CC': 0
+    }
+
+    for key in courses_dict.keys():
+        courses_dict[key] = len(data[key])
+
+    print(courses_dict.items())
+    return courses_dict
+
 
 # Display the statistical data of the passers
-def displayStatData():
-    pass   
+def displayStatData(course_dict):
+    degree_Programs = list(course_dict.keys()) 
+    numOfPassers = list(course_dict.values())
+    print(numOfPassers)
+    fig =  akonplotter.figure(figsize=(40, 40))
+
+    akonplotter.bar(degree_Programs, numOfPassers, color="blue", width=0.3)
+
+    akonplotter.title("EVSU Class of 2021-2022 Passers")
+    akonplotter.xlabel("Degree Programs")
+    akonplotter.ylabel("Number of Passers")
+    akonplotter.xticks(rotation=90)
+    
+    akonplotter.show()
 
 data = getJsonData()
-getStats(data)
+course_dict = getStats(data)
+displayStatData(course_dict)
