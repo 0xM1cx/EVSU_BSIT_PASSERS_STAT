c51b0721b9f43cddd4079df3534c2cfc9892a0cd
Shawn Sudaria <shawnlovecode14@gmail.com>
2024-05-03 16:47:20 +0800

    Update

--- getStatistics.py
+++ getStatistics.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as akonplotter
 import json
 import math
+import numpy as np
 '''
 AUTHOR: Shawn Michael A. Sudaria
 GITHUB: https://github.com/0xM1cx
@@ -24,68 +25,59 @@ def getJsonData():
     return json_data
 
 # Get the number of students per key in the json_data dict | temp
-def getStats(data):
+def getStats():
     # Program Generated List of courses found in the pdf files
     courses_dict = {
-        'BEED': 0, 
-        'BS Ind. Tech. GAP': 0,  
-        'BS Ind. Tech. E': 0, 
-        'BSF': 0,  
-        'BSMATH': 0, 
-        'BTVTED HVART': 0, 
-        'BSOA': 0, 
-        'BTVED AFA': 0,  
-        'BSIT': 0,  
-        'BSE': 0,  
-        'BS Ind. Tech. ELX': 0,  
-        'BAEL': 0,  
-        'BSID': 0,  
-        'BSCE': 0,  
-        'BSED SCI': 0, 
-        'BS Ind. Tech. CA': 0,  
-        'BPED': 0,  
-        'BSHM': 0, 
-        'BENS': 0,  
-        'BSIENG': 0,  
-        'BSND': 0,  
-        'BTVTED FSM': 0,  
-        'BSFi': 0, 
-        'BSARCH': 0,  
-        'BSMT AT': 0, 
-        'BSA': 0, 
-        'BSED MATH': 0, 
-        'BSEE': 0, 
-        'BSM': 0,  
-        'BSECON': 0, 
-        'BSME': 0, 
-        'BSCHE': 0, 
-        'BSECE': 0,  
-        'BTVTED GFD': 0, 
-        'BSAGRI': 0, 
-        'BSCHEM': 0, 
-        'BCAEd': 0, 
-        'BSSTAT': 0, 
-        'BTLED HE': 0, 
-        'BSE ENGLISH': 0, 
-        'BTLED IA': 0, 
-        'BSGE': 0, 
-        'BSMT MS': 0, 
-        'BS Ind. Tech. CC': 0,
-        'BTVED AT': 0,
-        'BTVED CCT': 0,
-        'BS Ind. Tech. RAC': 0,
-        'BTVED ET': 0,
-        'BS Ind. Tech. CFD': 0,
-        'BSMT WF': 0
+        'BEED': 200, 
+        'BIndTech AT': 50,  
+        'BIndTech CUL': 50, 
+        'BSF': 50,  
+        'BSMATH': 80, 
+        'BSOA': 135, 
+        'BSIT': 180,  
+        'BSES': 80,  
+        'BIndTech ELX': 50,  
+        'BAEL': 100,  
+        'BSID': 60,  
+        'BSCE': 160,  
+        'BSED SCI': 150, 
+        'BIndTech PMT': 50,  
+        'BPED': 150,  
+        'BSHM': 180, 
+        'BSND': 100,  
+        'BSARCH': 90,  
+        'BSA': 225, 
+        'BSED MATH': 150, 
+        'BSECON': 50, 
+        'BSCHE': 40, 
+        'BSCHEM': 35, 
+        'BCAEd': 100, 
+        'BSSTAT': 50, 
+        'BIndTech MT': 50,
+        'BIndTech ET': 50,
+        'BIndTech HVACRT': 50,
+        'BIndtTech WFT': 50,
+        'BSE': 135,
+        'BSBA MM': 90,
+        'BIndTech AFT': 50,
+        'BTLED HE': 150,
+        'BTLED IA': 150,
+        'BTVTED FSM': 100,
+        'BTVTED GFD': 100,
+        'BSME': 120,
+        'BSECE': 80,
+        'BSGE': 4,
+        'BSIENG': 80,
+        'BSEE': 120
     }
 
 
     # This loop gets the keys and appends the lengths of their
-    # value and storin them in the course_dict dictionary.
-    for key in courses_dict.keys():
-        courses_dict[key] = int(len(data[key]))
+    # value and stores them in the course_dict dictionary.
+    # for key in courses_dict.keys():
+    #     courses_dict[key] = int(len(data[key]))
 
-    print(courses_dict.items())
+    # print(courses_dict.items())
     return courses_dict
 
 # Display the statistical data of the passers
@@ -96,16 +88,23 @@ def displayStatData(course_dict):
     print(numOfPassers)
     # Draws the figure given the dimensions
     fig =  akonplotter.figure(figsize=(600, 40))
-    
-    akonplotter.barh(degree_Programs, numOfPassers, color="blue")
-    
-    akonplotter.title("EVSU Class of 2021-2022 Passers")
-    akonplotter.xlabel("Number of Passers", fontsize=5)
+    colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
+              '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf',
+              '#aec7e8', '#ffbb78', '#98df8a', '#ff9896', '#c5b0d5',
+              '#c49c94', '#f7b6d2', '#c7c7c7', '#dbdb8d', '#9edae5']
+    bars = akonplotter.barh(degree_Programs, numOfPassers, color=colors)
+
+    akonplotter.title("EVSU 2024-2025 Passers")
+    akonplotter.xlabel("Number of Passers", fontsize=10)
     akonplotter.ylabel("Degree Programs")
     akonplotter.xticks(rotation=90)
     
+    akonplotter.gca().xaxis.set_major_locator(akonplotter.MultipleLocator(10))
+    akonplotter.grid(axis='x', linestyle='--', alpha=0.7, color='black')
+    for bar, color in zip(bars, colors):
+        bar.set_color(color)
     akonplotter.show()
 
-data = getJsonData() # This is used to get the json data from the DATA.json file
-course_dict = getStats(data) # This is used to determine 
+# data = getJsonData() # This is used to get the json data from the DATA.json file
+course_dict = getStats() # This is used to determine 
 displayStatData(course_dict)
